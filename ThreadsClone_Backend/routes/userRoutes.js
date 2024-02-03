35d676a8a85989462b0f3cb8583a9654408633ba
Minela Ganovic <59210673+minelaganovic@users.noreply.github.com>
2024-02-04 00:02:37 +0100

    Add files via upload

---
+++ ThreadsClone_Backend/routes/userRoutes.js
@@ -0,0 +1,29 @@
+import express from "express";
+import {
+	followUnFollowUser,
+	getUserProfile,
+	loginUser,
+	logoutUser,
+	signupUser,
+	updateUser,
+	getSuggestedUsers,
+	freezeAccount,
+	getUserFollowers,
+	getUserFollowing
+} from "../controllers/userController.js";
+import protectRoute from "../middlewares/protectRoute.js";
+
+const router = express.Router();
+
+router.get("/profile/:query", getUserProfile);
+router.get("/suggested", protectRoute, getSuggestedUsers);
+router.get("/followers/:id",getUserFollowers);
+router.get("/following/:id",getUserFollowing);
+router.post("/signup", signupUser);
+router.post("/login", loginUser);
+router.post("/logout", logoutUser);
+router.post("/follow/:id", protectRoute, followUnFollowUser); // Toggle state(follow/unfollow)
+router.put("/update/:id",protectRoute, updateUser);
+router.put("/freeze", protectRoute, freezeAccount);
+
+export default router;
